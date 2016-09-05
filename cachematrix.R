fd5f01c5addc0e6dbf6dfe3bc06979fe8fe909f4
VeBeller <verena.beller@gmx.at>
2016-09-05 21:25:20 +1000

    Try 2nd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two function below can be used to calculate the inverse of a 2x2 matrix and cache the result for future use/reference
 
-## Write a short comment describing this function
+## The function will always first check the cache to see whether the mean has already beeen calculated and stored
+## If the mean has already been stored in the cache, the inverse is retrieved from the cache,
+## otherwise R will calculate the inverse using solve()
+
+## makeCacheMatrix returns a list of functions to set/get a matrix x, and set/get it's inverse matrix i
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i<-NULL
+        set<-function(y){
+          x<<-y
+          i<<-NULL
+        }
+        get<-function() x
+        setinv<-function(inv) i<<-inv
+        getinv<-function() i
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses an object created by makeCacheMatrix as input to calculate (or retrieve) the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i<-x$getinv()
+        if(!is.null(i)){
+          message("getting cached inverse matrix")
+          return(i)
+        }
+        data<-x$get()
+        i<-solve(data,...)
+        x$setinv(i)
+        i
 }
